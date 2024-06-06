51d014a57beedfc50bf05709c8d3de0d8e8a8d54
Hemant <hjkumar17@gmail.com>
2024-06-07 00:06:59 +0530

    react interview qns

---
+++ ReactInterviewQns.js
@@ -0,0 +1,55 @@
+// Q1 - React Optimization techniques ?
+
+// Q2 - How can you reduce initial load time of react application ?
+
+// Q3 - WCAG accessibility ?
+
+// Q4 - Custom react hooks ?
+
+// Q5 - Redux flow ( dispatch action to subscribing component, createStore,configureStore etc) ?
+
+// Q6 - SEO tags using to improve search index ?
+
+// Q7 - Cross browser commpatible ?
+
+// Q8 - Portals in react ?
+
+// Q9 - Server side rendering and client side rendering ?
+
+// Q10 - Server Side Scripting and Client Side Scripting ?
+
+// Q11 - CSS-In-JS or Styled Components, other ways to style react components ?
+
+// Q12 - Animation/ Particle animation in react ?
+
+// Q13 - Async/Await and Generator functions in react ? Are they similar if yes, how ?
+
+// Q14 - Static side generation in React/NextJS ?
+
+// Q15 - How can we avoid binding in React components ?
+
+// Q16 - React Context API ?
+
+// Q17 - Error Boundaries ?
+
+// Q18 - setState() vs useState() ?
+
+// Q19 - Can custom hooks return JSX ?
+
+// Q20 - ComponentWillUnmount and useEffect with callback return ?
+
+// Q21 - React Performace issue, how to improve and test performance of react components ?
+
+// Q22 - Context API or Redux which is suitable where ?
+
+// Q23 - How to implement SSR ?
+
+// Q24 - Constructor or getInitialState in react ?
+
+// Q25 - Testing React Application ?
+
+// Q26 - 
+
+
+
+// 
